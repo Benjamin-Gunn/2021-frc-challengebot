75f098fbb581f44a1b5f0f80217b033bdba731e1
Matthew <uni.oates@gmail.com>
2021-01-21 16:22:51 -0700

    Swerve pure pursuit working in Sim

--- src/main/java/org/team1619/behavior/Behavior_Drivetrain_Zero.java
+++ src/main/java/org/team1619/behavior/Behavior_Drivetrain_Zero.java
@@ -1,6 +1,5 @@
 package org.team1619.behavior;
 
-import org.uacr.models.behavior.Behavior;
 import org.uacr.shared.abstractions.InputValues;
 import org.uacr.shared.abstractions.OutputValues;
 import org.uacr.shared.abstractions.RobotConfiguration;
@@ -10,103 +9,81 @@ import org.uacr.utilities.logging.LogManager;
 import org.uacr.utilities.logging.Logger;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 /**
  * Zeros the swerve modules
  */
 
-public class Behavior_Drivetrain_Zero implements Behavior {
+public class Behavior_Drivetrain_Zero extends BaseSwerve {
 
-	private static final Logger sLogger = LogManager.getLogger(Behavior_Drivetrain_Zero.class);
-	private static final Set<String> sSubsystems = Set.of("ss_drivetrain");
+	private static final Logger LOGGER = LogManager.getLogger(Behavior_Drivetrain_Zero.class);
 
-	private final InputValues fSharedInputValues;
-	private final OutputValues fSharedOutputValues;
-
-	private final Timer fTimeoutTimer;
-	private int mTimeoutTime;
-	private double mZeroingThreshold;
+	private final Timer timeoutTimer;
+	private int timeoutTime;
+	private double zeroingThreshold;
 
 	public Behavior_Drivetrain_Zero(InputValues inputValues, OutputValues outputValues, Config config, RobotConfiguration robotConfiguration) {
-		fSharedInputValues = inputValues;
-		fSharedOutputValues = outputValues;
+		super(inputValues, outputValues, config, robotConfiguration);
 
-		fTimeoutTimer = new Timer();
-		mTimeoutTime = 500;
-		mZeroingThreshold = 0.1;
+		timeoutTimer = new Timer();
+		timeoutTime = 500;
+		zeroingThreshold = 0.1;
 	}
 
 	@Override
 	public void initialize(String stateName, Config config) {
-		sLogger.debug("Entering state {}", stateName);
-		mTimeoutTime = config.getInt("timeout_time");
-		mZeroingThreshold = config.getDouble("zeroing_threshold");
+		LOGGER.debug("Entering state {}", stateName);
 
-		fTimeoutTimer.reset();
-		fTimeoutTimer.start(mTimeoutTime);
+		timeoutTime = config.getInt("timeout_time");
+		zeroingThreshold = config.getDouble("zeroing_threshold");
 
-		fSharedOutputValues.setNumeric("opn_drivetrain_front_right_speed", "percent", 0);
-		fSharedOutputValues.setNumeric("opn_drivetrain_front_left_speed", "percent", 0);
-		fSharedOutputValues.setNumeric("opn_drivetrain_back_right_speed", "percent", 0);
-		fSharedOutputValues.setNumeric("opn_drivetrain_back_left_speed", "percent", 0);
+		timeoutTimer.reset();
+		timeoutTimer.start(timeoutTime);
 
-		fSharedOutputValues.setNumeric("opn_drivetrain_front_right_angle", "percent", 0);
-		fSharedOutputValues.setNumeric("opn_drivetrain_front_left_angle", "percent", 0);
-		fSharedOutputValues.setNumeric("opn_drivetrain_back_right_angle", "percent", 0);
-		fSharedOutputValues.setNumeric("opn_drivetrain_back_left_angle", "percent", 0);
+		stop();
+
+		sharedInputValues.setInputFlag(navx, "zero");
+
+		Stream.concat(angleOutputNames.stream(), speedOutputNames.stream()).forEach(output -> sharedOutputValues.setOutputFlag(output, "zero"));
 	}
 
 	@Override
 	public void update() {
-		if (!fSharedInputValues.getBoolean("ipb_drivetrain_has_been_zeroed")) {
+		if (!sharedInputValues.getBoolean("ipb_drivetrain_has_been_zeroed")) {
 
-			//Todo - Should we do this every frame or move them into Init()?
-			fSharedOutputValues.setOutputFlag("opn_drivetrain_front_right_angle", "zero");
-			fSharedOutputValues.setOutputFlag("opn_drivetrain_front_left_angle", "zero");
-			fSharedOutputValues.setOutputFlag("opn_drivetrain_back_right_angle", "zero");
-			fSharedOutputValues.setOutputFlag("opn_drivetrain_back_left_angle", "zero");
+			if (positionInputNames.stream().mapToDouble(input -> Math.abs(sharedInputValues.getNumeric(input))).max().getAsDouble() < zeroingThreshold &&
+				Math.abs(sharedInputValues.getVector(navx).get("angle")) < zeroingThreshold) {
+				LOGGER.debug("Drivetrain Zero -> Zeroed");
 
-			if ((Math.abs(fSharedInputValues.getNumeric("ipn_drivetrain_front_right_angle_position")) < mZeroingThreshold)
-				&& (Math.abs(fSharedInputValues.getNumeric("ipn_drivetrain_front_left_angle_position")) < mZeroingThreshold)
-				&& (Math.abs(fSharedInputValues.getNumeric("ipn_drivetrain_back_left_angle_position")) < mZeroingThreshold)
-				&& (Math.abs(fSharedInputValues.getNumeric("ipn_drivetrain_back_left_angle_position")) < mZeroingThreshold))
-			{
-				sLogger.debug("Drivetrain Zero -> Zeroed");
-				//Todo - Is this correct? What is it for? Why is it false?
-				//fSharedInputValues.setBoolean("ipb_odometry_has_been_zeroed", false);
-				fSharedInputValues.setBoolean("ipb_drivetrain_has_been_zeroed", true);
+				sharedInputValues.setInputFlag(odometry, "zero");
+
+				Map<String, Double> odometryValues = sharedInputValues.getVector(odometry);
+
+				if(Math.abs(odometryValues.get("x")) < zeroingThreshold &&
+						Math.abs(odometryValues.get("y")) < zeroingThreshold) {
+					sharedInputValues.setBoolean("ipb_drivetrain_has_been_zeroed", true);
+				}
 				return;
 			}
 
-			if (fTimeoutTimer.isDone()) {
-				sLogger.error("Drivetrain Zero -> Timed Out");
-				fTimeoutTimer.reset();
-				fSharedInputValues.setBoolean("ipb_drivetrain_has_been_zeroed", true);
+			if (timeoutTimer.isDone()) {
+				LOGGER.error("Drivetrain Zero -> Timed Out");
+
+				timeoutTimer.reset();
+				sharedInputValues.setInputFlag(odometry, "zero");
+				sharedInputValues.setBoolean("ipb_drivetrain_has_been_zeroed", true);
 			}
 		}
 	}
 
 	@Override
 	public void dispose() {
-		//Todo - Do we need this as they are already set this way?
-		fSharedOutputValues.setNumeric("opn_drivetrain_front_right_speed", "percent", 0);
-		fSharedOutputValues.setNumeric("opn_drivetrain_front_left_speed", "percent", 0);
-		fSharedOutputValues.setNumeric("opn_drivetrain_back_right_speed", "percent", 0);
-		fSharedOutputValues.setNumeric("opn_drivetrain_back_left_speed", "percent", 0);
 
-		fSharedOutputValues.setNumeric("opn_drivetrain_front_right_angle", "percent", 0);
-		fSharedOutputValues.setNumeric("opn_drivetrain_front_left_angle", "percent", 0);
-		fSharedOutputValues.setNumeric("opn_drivetrain_back_right_angle", "percent", 0);
-		fSharedOutputValues.setNumeric("opn_drivetrain_back_left_angle", "percent", 0);
 	}
 
 	@Override
 	public boolean isDone() {
-		return fSharedInputValues.getBoolean("ipb_drivetrain_has_been_zeroed") || fTimeoutTimer.isDone();
-	}
-
-	@Override
-	public Set<String> getSubsystems() {
-		return sSubsystems;
+		return sharedInputValues.getBoolean("ipb_drivetrain_has_been_zeroed");
 	}
 }
